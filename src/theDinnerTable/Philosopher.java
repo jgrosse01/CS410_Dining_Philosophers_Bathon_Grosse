3938543dd5e7a5fc20800f52b6eaa42cc0908f6b
Jake Grosse <jgrosse@carroll.edu>
2021-10-04 10:53:02 -0600

    More philosopher methods

--- src/theDinnerTable/Philosopher.java
+++ src/theDinnerTable/Philosopher.java
@@ -1,13 +1,45 @@
 package theDinnerTable;
 
 public class Philosopher implements Runnable{
+	
+	private enum State {
+		Thinking(randomThinkingTime()),
+		// negative times to indicate that it doesn't matter, must incorporate in code
+		Hungry(-1),
+		Eating(-1);
+		
+		// random thinking time between 0 and 10 seconds
+		private static int randomThinkingTime() {
+			return (int)Math.random()*10000;
+		}
+		
+		final int thinkTime;
+		
+		private State(int thinkTime) {
+			this.thinkTime = thinkTime;
+		}
+	}
+	
+	private State state;
 
 	public Philosopher() {
-		
+		state = State.Thinking;
+	}
+	
+	public State getState() { 
+		return state;
 	}
 	
 	@Override
 	public void run() {
+		
+	}
+	
+	public void think() {
+		
+	}
+	
+	public void hungry() {
 		
 	}
 	
