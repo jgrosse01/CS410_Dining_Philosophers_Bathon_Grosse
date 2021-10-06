d7d830dfb3ff873f012fba3ed160af1c54eba95a
Jaden Bathon <jbathon@carroll.edu>
2021-10-06 11:50:38 -0600

    Made Philosophers wait on Chopsticks

--- src/theDinnerTable/Table.java
+++ src/theDinnerTable/Table.java
@@ -1,5 +1,7 @@
 package theDinnerTable;
 
+import java.util.ArrayList;
+
 public class Table {
 	
 	private static final int NUM_PHILS = 5;
@@ -14,44 +16,43 @@ public class Table {
 	}
 	
 	private Philosopher[] phils;
-	private Chopstick[] chopsticks;
+	private ArrayList<Chopstick> chopsticks;
 	private int riceBowl;
 	
 	Table() {
 		phils = new Philosopher[NUM_PHILS];
-		chopsticks = new Chopstick[NUM_PHILS];
+		chopsticks = new ArrayList<Chopstick>();
 		for (int i = 0; i < NUM_PHILS; i++) {
 			phils[i] = new Philosopher(this, i);
-			chopsticks[i] = new Chopstick(i);
+			chopsticks.add(new Chopstick(i));
 			riceBowl = OUNCES_OF_RICE;
 		}
 	}
 	
-	public Chopstick[] findChopsticks(Philosopher p) {
+	public ArrayList<Chopstick> findChopsticks(Philosopher p) {
 		final int pos = p.getPos();
 		final int[] range = posRange(pos);
-		while (true) {
-			Chopstick[] chopsticks = new Chopstick[2];
+		ArrayList<Chopstick> chops = new ArrayList<Chopstick>();
+		while (chops.size() < 2) {
 			for (int i : range) {
-				if(chopsticks.length <= 2)
-					if(!this.chopsticks[i].isInUse()) {
-						chopsticks[i] = this.chopsticks[i];
+				if(!this.chopsticks.get(i).isInUse()) {
+					chops.add(chopsticks.get(i));
+				} else {
+					synchronized (chopsticks) {
+						try {
+							chopsticks.wait();
+						} catch (InterruptedException ignored) {}
 					}
-			}
-			if (chopsticks.length != 2) {
-				try {
-					p.wait(100);
-				} catch (InterruptedException ignored) {}
-			} else {
-				for(Chopstick c : chopsticks) {
-					c.pickUp();
 				}
-				return chopsticks;
 			}
 		}
+		return chopsticks;
 	}
 	
 	public void placeChopsticks(Chopstick[] chopstick) {
+		synchronized (chopsticks) {
+			chopsticks.notifyAll();
+		}
 		for(Chopstick c : chopsticks) {
 			c.setDown();
 		}
